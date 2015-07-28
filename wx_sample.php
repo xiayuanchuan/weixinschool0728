cec85ccac58fc1d9fbe35ae84050ac656006fc79
xiayuanchuan <xiayuanchuan@bravesoft.co.jp>
2015-07-29 01:22:54 +0800

    添加jsdemo及页面
    Signed-off-by: xiayuanchuan <xiayuanchuan@bravesoft.co.jp>

--- wx_sample.php
+++ wx_sample.php
@@ -1,100 +1,97 @@
 <?php
-/**
-  * wechat php test
-  */
 
+/**
+ * wechat php test
+ */
 //define your token
 define("TOKEN", "weixin");
 $wechatObj = new wechatCallbackapiTest();
 $wechatObj->valid();
 
-class wechatCallbackapiTest
-{
-	public function valid()
-    {
+class wechatCallbackapiTest {
+
+    public function valid() {
         $echoStr = $_GET["echostr"];
 
         //valid signature , option
-        if($this->checkSignature()){
-        	echo $echoStr;
-            
-            
+        if ($this->checkSignature()) {
+            echo $echoStr;
+
+
             file_put_contents("./wx_sam.txt", $echoStr);
             $this->responseMsg();
 //            file_put_contents("./wx_samresponseMsg.txt", $echoStr);
-            
-            
-        	exit;
+
+
+            exit;
         }
     }
 
-    public function responseMsg()
-    {
-		//get post data, May be due to the different environments
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+    public function responseMsg() {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-           file_put_contents("./wx_samresponseMsg.txt", json_encode($GLOBALS));
+        file_put_contents("./wx_samresponseMsg.txt", json_encode($GLOBALS));
+        file_put_contents("./wx_samresponseMsgpostStr.txt", json_encode($postStr));
 
-      	//extract post data
-		if (!empty($postStr)){
-                /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-                   the best way is to check the validity of xml by yourself */
-                libxml_disable_entity_loader(true);
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $MsgType = trim($postObj->MsgType);
-                $time = time();
-                $textTpl = "<xml>
+        //extract post data
+        if (!empty($postStr)) {
+            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+              the best way is to check the validity of xml by yourself */
+            libxml_disable_entity_loader(true);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $MsgType = trim($postObj->MsgType);
+            $time = time();
+            $textTpl = "<xml>
 							<ToUserName><![CDATA[%s]]></ToUserName>
 							<FromUserName><![CDATA[%s]]></FromUserName>
 							<CreateTime>%s</CreateTime>
 							<MsgType><![CDATA[%s]]></MsgType>
 							<Content><![CDATA[%s]]></Content>
 							<FuncFlag>0</FuncFlag>
-							</xml>";             
-				if(!empty( $keyword ))
-                {
-                    //最好是用$MsgType来判断， f否则有可能无法处理用户的其他输入
-              		$msgType = "text";
-                	$contentStr = "Welcome to wechat world!您的输入类型为：".$MsgType;
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
-                }else{
-                	echo "Input something...";
-                }
-
-        }else {
-        	echo "";
-        	exit;
+							</xml>";
+            if (!empty($keyword)) {
+                //最好是用$MsgType来判断， f否则有可能无法处理用户的其他输入
+                $msgType = "text";
+                $contentStr = "Welcome to wechat world!您的输入类型为：" . $MsgType . $keyword;
+            } else {
+                $contentStr = "Welcome to wechat world!您的输入类型为：" . $MsgType . $keyword;
+            }
+            $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+            echo $resultStr;
+        } else {
+            echo "";
+            exit;
         }
     }
-		
-	private function checkSignature()
-	{
+
+    private function checkSignature() {
         // you must define TOKEN by yourself
         if (!defined("TOKEN")) {
             throw new Exception('TOKEN is not defined!');
         }
-        
+
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
-        		
-		$token = TOKEN;
-		$tmpArr = array($token, $timestamp, $nonce);
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
         // use SORT_STRING rule
-		sort($tmpArr, SORT_STRING);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
-		
-		if( $tmpStr == $signature ){
-			return true;
-		}else{
-			return false;
-		}
-	}
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
 
 ?>
