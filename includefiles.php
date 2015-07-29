4f728fbd902b9e1a97152bd77a786423b8ef0c0b
xiayuanchuan <xiayuanchuan@bravesoft.co.jp>
2015-07-29 20:19:49 +0800

    inclu files.php
    Signed-off-by: xiayuanchuan <xiayuanchuan@bravesoft.co.jp>

--- includefiles.php
+++ includefiles.php
@@ -6,10 +6,3 @@ define("APP_PATH", "");
 include './Common/config/config.php';
 include './Common/mysql/mysql.php';
 include './Common/functions/functions.php';
-
-//
-//$mydb=new mysql();
-//$sql="select * from weixin_user";
-//$res=$mydb->execute($sql);
-////var_dump($mydb->fetch_array($res));
-global $access_token = getAccessToken(1);
